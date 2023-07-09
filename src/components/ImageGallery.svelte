1ac0b41ac8cee0e69449982dc6fea5b8e4bf02f2
jessvoiture <jessicascarr989@gmail.com>
2023-07-09 14:17:32 +0100

    resizing

--- src/components/ImageGallery.svelte
+++ src/components/ImageGallery.svelte
@@ -10,42 +10,6 @@
                     .sort((a, b) => a.Date.localeCompare(b.Date))
                     .slice(100, 102);
 
-  // browser dimensions
-  let innerWidth; // you can also set your default width
-  let innerHeight;
-
-  const updateWindowSize = () =>{
-      innerWidth = window.innerWidth;
-      innerHeight = window.innerHeight;
-  }
-  if (browser){
-      updateWindowSize() // to set the initial window size
-      window.onresize = updateWindowSize; // run when ever the window size change
-      
-  }
-
-  // dom elements dimensions
-  let width;
-  let mag_width;
-  let mag_height;
-  let padding = 30;
-  let dist_from_top;
-
-  $: if (innerWidth > 800) {
-      width = 0.8 * innerWidth
-      // lets define the magazine dimensions by its height so they are nicely aligned
-      // however the mags should be displayed on one line (unless small screen)
-      // dimensions of the magazines are roughly W:3/H:4
-      mag_width = (width - padding) / sortedMagazines.length;
-      mag_height = mag_width * 4 / 3;
-      dist_from_top = (innerHeight - mag_height) / 2;
-
-    } else {
-      width = 0.95 * innerWidth;
-      mag_height = (innerHeight - 3 * padding) / 2;
-      dist_from_top = padding;
-    }
-
   // const toggleImage = (event) => {
   //   const img = event.target;
   //   const annotatedPath = img.dataset.annotatedPath;
@@ -56,16 +20,14 @@
 
   // scroll
   let currentStep = 0;
-  const steps = ["",
-                "<p>these are the original images</p>", 
+  const steps = ["<p>these are the original images</p>", 
                  "<p>these are the annotations</p>",
                 ""];
 
   let show_annotated = false;
 
-  $: if (currentStep >= 2) {
+  $: if (currentStep >= 1) {
       show_annotated = true;
-      console.log(show_annotated)
     } else {
       show_annotated = false;
     }
@@ -81,11 +43,8 @@
 
 <!-- <svelte:window bind:innerWidth={browserWidth} bind:innerHeight={browserHeight}/> -->
 
-<p>{innerWidth}</p>
-<p>{mag_height}</p>
-
-<section>
-  <div class="mag-gallery" {width} style="top:{dist_from_top}px" >
+<div class="scroller">
+  <div class="mag-gallery" >
     <div class="mag-wrapper">
       <div class='all-original-covers all-covers'>
         {#each sortedMagazines as magazine}
@@ -94,7 +53,6 @@
             class="original-cover cover"
             src={getImagePath(magazine.Date, "original", ".jpg")} 
             alt='Vogue magazine cover from {magazine.Date}'
-            height={mag_height} 
           />
         {/each}
       </div>
@@ -107,7 +65,6 @@
               class="annotated-cover cover"
               src={getImagePath(magazine.Date, "annotated", "-01.png")} 
               alt='A vogue magazine cover from {magazine.Date} (ie the same one displayed earlier in the page) BUT the text areas on the cover are covered by semi-opaque black rectangles and the rest of the cover is a transparent white'
-              height={mag_height}
             />
           {/each}
         </div>
@@ -118,7 +75,7 @@
 
   <Scroll bind:value={currentStep}>
     {#each steps as text, i}
-      <div class="step {currentStep == 3 || currentStep == 0 ? "hidden-step" : ""}" 
+      <div class="step {currentStep == 2 ? "hidden-step" : ""}" 
            class:active={currentStep === i}>
         <div class="step-content">
           {@html text}
@@ -126,7 +83,7 @@
       </div>
     {/each}
   </Scroll>
-</section>
+</div>
 
 <style>
   .step {
@@ -162,20 +119,8 @@
     position: sticky;
     margin: auto;
 		flex: 1 1 60%;
-  }
-
-  /* big gorl screens */
-  @media screen and (min-width: 860px) {
-    .mag-gallery {
-      top: 20vh
-    }
-  }
-
-  /* small screens */
-  @media screen and (max-width: 860px) {
-    .mag-gallery {
-      top: 30px
-    }
+    top: 0vh;
+    height: 60vh;
   }
 
   .mag-wrapper {
@@ -199,6 +144,10 @@
 
   .all-covers {
     flex-direction: column
+  }
+
+  .cover {
+    height: 50vh;
   }
 
 </style>
