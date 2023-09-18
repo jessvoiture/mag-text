a798964493dbae4a771fca0b66b1dabb88497342
jessvoiture <jessicascarr989@gmail.com>
2023-09-18 23:07:08 +0200

    sorting

--- src/components/MagazineList.svelte
+++ src/components/MagazineList.svelte
@@ -1,13 +1,14 @@
 <script>
-  import { fade } from "svelte/transition";
   import { months } from "../stores";
+
   export let data;
   export let screenWidth;
 
   let width;
+  let sortBy;
 
-  let showingAnnotations = false;
-  let displayingCount = 3;
+  let ascending = true;
+  let magWidth = 100;
 
   let mags = data.magazines;
   let sortedMagazinesRatio = [...mags].sort((a, b) => b.ratio - a.ratio);
@@ -15,38 +16,66 @@
   $: if (screenWidth <= 860) {
     width = 0.9 * screenWidth;
   } else {
-    width = 0.6 * screenWidth;
+    width = 0.7 * screenWidth;
   }
+
+  $: displayingCount = 9;
 
   const getImagePath = (path, image_type, ending) =>
     `/${image_type}/${path}${ending}`;
+
+  function sort(by) {
+    if (by === sortBy) {
+      ascending = !ascending;
+    } else {
+      ascending = true;
+    }
+
+    sortedMagazinesRatio = [...mags].sort((a, b) => {
+      if (by === "year") {
+        return ascending ? a.year - b.year : b.year - a.year;
+      } else if (by === "ratio") {
+        return ascending ? b.ratio - a.ratio : a.ratio - b.ratio;
+      }
+    });
+
+    sortBy = by;
+  }
 </script>
 
-<div class="magazine-list" style="width: {width}px;">
-  {#each sortedMagazinesRatio.slice(0, displayingCount) as d}
-    <div class="list-element">
-      <div class="list-img">
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-        <img
-          class="original-image"
-          src={getImagePath(d.Date, "original", ".jpg")}
-          alt="Vogue cover"
-        />
+<div class="sort-buttons">
+  <button on:click={() => sort("year")}>Sort by Year</button>
+  <button on:click={() => sort("ratio")}>Sort by Ratio</button>
+</div>
+
+<div class="list-wrapper" style="width: {width}px;">
+  <div class="magazine-list">
+    {#each sortedMagazinesRatio.slice(0, displayingCount) as d}
+      <div class="list-element" style="width: {magWidth}px;">
+        <div class="list-img">
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+          <img
+            class="original-image"
+            style="width: {magWidth}px; height: auto;"
+            src={getImagePath(d.Date, "original", ".jpg")}
+            alt="Vogue cover"
+          />
+        </div>
+        <div class="meta-info">
+          <div class="body-text">{d.year} — {$months[d.month - 1]}</div>
+          <div class="body-text">{Math.round(d.ratio * 100)}%</div>
+        </div>
       </div>
-      <div class="meta-info">
-        <div class="body-text">{$months[d.month - 1]}, {d.year}</div>
-        <div class="body-text">{Math.round(d.ratio * 100)}%</div>
-      </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 
   {#if displayingCount < mags.length}
     <button
       on:click={() => (displayingCount = displayingCount + 3)}
       id="loadmore"
       type="button"
-      class="btn btn-secondary"
+      class="see-more-btn"
     >
       Show more
     </button>
@@ -54,35 +83,40 @@
 </div>
 
 <style>
+  .list-wrapper {
+    margin: auto;
+  }
   .magazine-list {
-    display: grid;
-    grid-template-columns: repeat(
-      auto-fill,
-      minmax(200px, 1fr)
-    ); /* Adjust the minmax values to control the column width */
-    gap: 10px; /* Adjust the gap between items */
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: space-between;
+    justify-content: space-between;
+    width: 100%;
     margin: auto;
   }
 
   .list-element {
-    padding: 10px; /* Add padding to the elements for some spacing */
+    padding: 10px;
   }
 
   .body-text {
     margin-bottom: 5px; /* Add margin to separate the text elements */
   }
 
-  img {
-    width: 100%;
-    height: auto;
-  }
-
-  .meta-info {
-    display: flex;
-    justify-content: space-between;
-  }
-
   .original-image {
     position: relative;
+  }
+
+  .sort-buttons {
+    margin-bottom: 10px;
+  }
+
+  .sort-buttons button {
+    margin-right: 10px;
+  }
+
+  .see-more-btn {
+    margin: auto;
   }
 </style>
