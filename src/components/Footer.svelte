dc69a191f320de75d92d33e1490a6897fad49c15
jessvoiture <jessicascarr989@gmail.com>
2023-09-22 21:51:15 +0200

    removed footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Colophon from "./Colophon.svelte";
+</script>
+
+<div class="footer">
+  <div class="section">
+    <Colophon />
+  </div>
+
+  <div class="section">
+    <Colophon />
+  </div>
+</div>
+
+<style>
+  .footer {
+    background-color: #221919;
+    padding-top: 5vh;
+    padding-bottom: 5vh;
+  }
+</style>
